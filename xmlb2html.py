39e5c7d7137e6ba4eb9d8d45d91b2e468aa6676b
Jonathan Patt <jonathanpatt@gmail.com>
2011-03-23 11:59:00 -0700

    Cleaning up variable naming and some CSS glitches

--- xmlb2html.py
+++ xmlb2html.py
@@ -12,7 +12,45 @@ import time
 
 footnotes = {}
 footnote_id = 0
-head = [u"<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"\n  \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">\n<head>\n <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"/>\n  <title>", "</title>\n<style type=\"text/css\" media=\"screen\">h1, h2, h3, p#subtitle, p.subtitle, p#author, p#publication_date, p#title, p.title, div#dedication { text-align: center; } p, p.title + p, p.subtitle + p, p#author, p#publication_date, p#title, p#subtitle { margin: 0; text-indent: 0; } p + p { text-indent: 1.5em; } p.subtitle + p, { text-indent: 0 } h1 + div.quote, p.title + div.quote, p.subtitle + div.quote { margin-top: 2em, margin-bottom: 2em } div.quote p.source, div.inter_quote p.source { text-align: right; } p#subtitle, p.subtitle { font-variant: small-caps; margin-bottom: 1em } .smallcaps { font-variant: small-caps; } img { display: block; margin-left: auto; margin-right: auto; } div#dedication, p#publication_date, p#author { margin-top: 2em; margin-bottom: 2em } br.page_break { page-break-after: always; } h1 { margin-top: 0px } sup { font-size: 0.75em; line-height: 0.5em } sub { font-size: 0.75em; line-height: .75em } p.aligned + p { text-indent: 0 } p.outdent { margin-left: 1.5em; text-indent: -1.5em !important } div.inter_quote { font-size: .8em; margin-top: 1em; margin-bottom: 1em; text-align: justify; } div.quote { margin-top: 1em; margin-bottom: 1em; text-align: justify; margin-left: 1.25em; margin-right: 1.25em } .section_glyph { padding-top: 60px; text-align: center; font-size: 40px; text-indent: 0px; } </style>\n</head>\n<body>\n    "]
+head = [u'<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"\n'
+            '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">\n'
+            '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" '
+                'lang="en">\n'
+            '<head>\n<meta http-equiv="Content-Type" '
+                'content="text/html;charset=utf-8"/>\n'
+            '<title>',
+         '</title>\n<style type="text/css" media="screen">'
+            'h1,h2,h3,h4,p.subtitle,p.byline,p.publication_date,'
+                'p.dedication{text-align:center;}'
+            'h1 + p.subtitle{margin-bottom:2em;}'
+            'h2 + p.subtitle,h3 + p.subtitle{margin-bottom:1em;'
+                'font-variant:small-caps;text-transform:lowercase;}'
+            'p.byline{font-size:1.2em;}'
+            'p.byline span.author{font-variant:small-caps;'
+                'text-transform:lowercase;}'
+            'p.publication_date{margin-top:4em;}'
+            'p,p.subtitle + p{margin:0;text-indent:0;}'
+            'p + p{text-indent:1em;}'
+            'p.byline,p.subtitle,p.publication_date,p.dedication{'
+                'text-indent:0;}'
+            'span.smallcaps{font-variant:small-caps;}'
+            'img{display:block;margin-left:auto;margin-right:auto;'
+                'text-align:center;}'
+            'br.page_break{page-break-after:always;}'
+            'sup{font-size:0.75em;line-height:0.5em}'
+            'sub{font-size:0.75em;line-height:.75em}'
+            'p.aligned + p{text-indent:0}'
+            'p.outdent{margin-left:1.5em;'
+            'text-indent:-1.5em !important}'
+            'table.inter_quote_table{margin-top:1em;margin-bottom:1em}'
+            'div.inter_quote{font-size:.8em;'
+                'text-align:justify;}'
+            'div.quote{margin-top:1em;margin-bottom:1em;text-align:justify;'
+                'margin-left:1.25em;margin-right:1.25em}'
+            'p.source{text-align: right}'
+            '.section_glyph{padding-top:60px;text-align:center;font-size:40px;'
+                'text-indent:0px;}'
+            '</style>\n</head>\n<body>\n']
 
 # CONSTANTS
 PAGE_BREAK = '<br class="page_break"/>'
@@ -40,27 +78,35 @@ def setBookInfo(topLevelElement, topLevelElementName):
     
     # Title
     try:
-        title = getChildrenByTagName(topLevelElement, 'title')
-        bookInfoOutput.append(head[0] + d(title) + head[1])
-        bookInfoOutput.append('<h1 id="title">' + handlePChildren(title[0].childNodes) + '</h1>')
+        titleElement = getChildrenByTagName(topLevelElement, 'title')
+        bookInfoOutput.append(head[0] + d(titleElement) + head[1])
+        
+        title = handlePChildren(titleElement[0].childNodes)
+        bookInfoOutput.append('<h1>' + title + '</h1>')
     except:
         exit("Error: Missing " + topLevelElementName + " <title> tag")
     
     # Subtitle if it exists
-    subtitle = getChildrenByTagName(topLevelElement, 'subtitle')
-    if d(subtitle):
-        bookInfoOutput.append('<p id="subtitle">' + handlePChildren(subtitle[0].childNodes) + '</p>')
+    subtitleElement = getChildrenByTagName(topLevelElement, 'subtitle')
+    if d(subtitleElement):
+        subtitle = handlePChildren(subtitleElement[0].childNodes)
+        bookInfoOutput.append('<p class="subtitle">' + subtitle + '</p>')
     
     # Author
     try:
-        bookInfoOutput.append('<p id="author"><i>by</i> ' + handlePChildren(getChildrenByTagName(topLevelElement, 'author')[0].childNodes) + '</p>')
+        authorElement = getChildrenByTagName(topLevelElement, 'author')
+        author = handlePChildren(authorElement[0].childNodes)
+        bookInfoOutput.append('<p class="byline"><i>by</i> '
+                              '<span class="author">' + author + '</span>'
+                              '</p>')
     except:
         exit("Error: Missing " + topLevelElementName + " <author> tag")
     
     # Publication date
     try:
-        publicationDate = d(getChildrenByTagName(topLevelElement, 'publication_date'))
-        publicationDate = publicationDate.split('-')
+        publicationDateElement = getChildrenByTagName(topLevelElement,
+                                                      'publication_date')
+        publicationDate = d(publicationDateElement).split('-')
         
         if len(publicationDate) == 1:
             publicationDate = publicationDate[0]
@@ -71,14 +117,22 @@ def setBookInfo(topLevelElement, topLevelElementName):
         else:
             raise
             
-        bookInfoOutput.append('<p id="publication_date">' + publicationDate + '</p>')
+        bookInfoOutput.append('<p class="publication_date">' +
+                              publicationDate +
+                              '</p>')
     except:
         exit('Error: Missing or invalid ' + topLevelElementName + ' <publication_date> tag')
     
     # Dedication if it exists
-    dedication = getChildrenByTagName(topLevelElement, 'dedication')
-    if dedication:
-        bookInfoOutput.append('<div id="dedication">' + handlePChildren(dedication[0].childNodes) + '</div>' + PAGE_BREAK)
+    dedicationElement = getChildrenByTagName(topLevelElement, 'dedication')
+    
+    if dedicationElement:
+        dedication = handlePChildren(dedicationElement[0].childNodes)
+        bookInfoOutput.append(PAGE_BREAK)
+        bookInfoOutput.append('<p class="dedication">' +
+                              dedication +
+                              '</p>')
+        bookInfoOutput.append(PAGE_BREAK)
     
     # Quotes if they exist
     if topLevelElementName == 'volume':
@@ -128,17 +182,21 @@ def handlePart(part):
     
     # Number
     if part.getAttribute('num'):
-        partOutput.append('<h1 class="part_title">Part ' + part.getAttribute('num') + '</h1>')
+        partOutput.append('<h2>Part ' + part.getAttribute('num') + '</h2>')
     else:
         exit("Error: Missing num attribute in <part> tag")
     
     # Title if it exists
-    if getChildrenByTagName(part, 'title'):
-        partOutput.append('<p class="subtitle">' + handlePChildren(getChildrenByTagName(part, 'title')[0].childNodes) + '</p>')
+    titleElement = getChildrenByTagName(part, 'title')
+    if titleElement:
+        title = handlePChildren(titleElement[0].childNodes)
+        partOutput.append('<h3>' + title + '</h3>')
     
     # Subtitle if it exists
-    if getChildrenByTagName(part, 'subtitle'):
-        partOutput.append('<p class="subtitle">' + handlePChildren(getChildrenByTagName(part, 'subtitle')[0].childNodes) + '</p>')
+    subtitleElement = getChildrenByTagName(part, 'subtitle')
+    if subtitleElement:
+        subtitle = handlePChildren(subtitleElement[0].childNodes)
+        partOutput.append('<p class="subtitle">' + subtitle + '</p>')
     
     # Quotes if they exist
     if getChildrenByTagName(part, 'quote'):
@@ -159,17 +217,25 @@ def handleChapter(chapter):
     
     # Number, title and subtitle
     headingOutput = []
-    if chapter.getAttribute('num'):
-        headingOutput.append('<h1 class="chapter_title">Chapter ' + chapter.getAttribute('num') + '</h1>')
-        if getChildrenByTagName(chapter, 'title'):
-            headingOutput.append('<p class="subtitle">' + handlePChildren(getChildrenByTagName(chapter, 'title')[0].childNodes) + '</p>')
-        if getChildrenByTagName(chapter, 'subtitle'):
-            headingOutput.append('<p class="subtitle">' + handlePChildren(getChildrenByTagName(chapter, 'subtitle')[0].childNodes) + '</p>')
+    
+    num = chapter.getAttribute('num')
+    titleElement = getChildrenByTagName(chapter, 'title')
+    subtitleElement = getChildrenByTagName(chapter, 'subtitle')
+    
+    if num:
+        headingOutput.append('<h2>Chapter ' +
+                chapter.getAttribute('num') + '</h2>')
+        if titleElement:
+            headingOutput.append('<h3>' + 
+                    handlePChildren(titleElement[0].childNodes) + '</h3>')
     else:
-        if getChildrenByTagName(chapter, 'title'):
-            headingOutput.append('<h1 class="chapter_title">' + handlePChildren(getChildrenByTagName(chapter, 'title')[0].childNodes) + '</h1>')
-        if getChildrenByTagName(chapter, 'subtitle'):
-            headingOutput.append('<p class="subtitle">' + handlePChildren(getChildrenByTagName(chapter, 'subtitle')[0].childNodes) + '</p>')
+        if titleElement:
+            headingOutput.append('<h2>' + 
+                    handlePChildren(titleElement[0].childNodes) + '</h2>')
+    
+    if subtitleElement:
+        headingOutput.append('<p class="subtitle">' +
+                handlePChildren(subtitleElement[0].childNodes) + '</p>')
     
     # Add forced page break if chapter has no heading
     if not headingOutput:
@@ -195,25 +261,27 @@ def handleChapterInternals(chapter, level = 1):
                     exit("Error: Invalid location for a subtitle")
             elif e.nodeName == 'title':
                 if level > 1:
-                    chapterInternalsOutput.append('<h3>' + handlePChildren(e.childNodes) + '</h3>')
+                    chapterInternalsOutput.append('<h4>' + 
+                            handlePChildren(e.childNodes) + '</h4>')
             elif e.nodeName == 'p':
                 pAttributes = []
                 if e.getAttribute('align'):
-                    pAttributes.append(' class="aligned" align="' + e.getAttribute('align') + '"')
+                    pAttributes.append(' class="aligned" align="' +
+                            e.getAttribute('align') + '"')
                 
                 if e.getAttribute('outdent'):
                     if e.getAttribute('outdent') == 'true':
                         pAttributes.append(' class="outdent"')
                 
-                chapterInternalsOutput.append('<p' + ''.join(pAttributes) + '>')
-                
+                chapterInternalsOutput.append('<p' +
+                        ''.join(pAttributes) + '>')
                 chapterInternalsOutput.append(handlePChildren(e.childNodes))
-                
                 chapterInternalsOutput.append('</p>')
             elif e.nodeName == 'quote':
                 chapterInternalsOutput.append(handleQuote(e))
             elif e.nodeName == 'code':
-                chapterInternalsOutput.append('<pre>' + e.firstChild.data +'</pre>')
+                chapterInternalsOutput.append('<pre>' +
+                        e.firstChild.data +'</pre>')
             elif e.nodeName == 'table':
                 chapterInternalsOutput.append(handleTable(e))
             elif e.nodeName == 'hr':
@@ -237,7 +305,8 @@ def handleTable(table):
             attributes = ''
             if column.getAttribute('align'):
                 attributes += ' align="' + column.getAttribute('align') + '"'
-            tableOutput.append('<td valign="top"' + attributes + '>' + handlePs(column) + '</td>')
+            tableOutput.append('<td valign="top"' + attributes + '>' + 
+                    handlePs(column) + '</td>')
         tableOutput.append('</tr>')
     tableOutput.append('</table>')
     
@@ -249,7 +318,9 @@ def handleQuote(quote):
     
     if quote.getAttribute('type'):
         if quote.getAttribute('type') == 'inter':
-            quoteOutput.append('<table width="100%"><tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td><td><div class="inter_quote">')
+            quoteOutput.append('<table width="100%" class="inter_quote_table">'
+                    '<tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>'
+                    '<td><div class="inter_quote">')
         else:
             exit("Error: Unknown <quote> type")
     else:
@@ -260,11 +331,14 @@ def handleQuote(quote):
     # Quote source if it exists
     source = getChildrenByTagName(quote, 'source')
     if source:
-        quoteOutput.append(u'<p class="source">—' + handlePChildren(source[0].childNodes) + '</p>')
+        quoteOutput.append(u'<p class="source">—' +
+                handlePChildren(source[0].childNodes) + '</p>')
     
     if quote.getAttribute('type'):
         if quote.getAttribute('type') == 'inter':
-            quoteOutput.append('</div></td><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td></tr></table>')
+            quoteOutput.append('</div></td>'
+                    '<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>'
+                    '</tr></table>')
     else:
         quoteOutput.append('</div>')
     
@@ -278,9 +352,10 @@ def handlePs(ps, attributes = '', prefix = ''):
         attributes = ' ' + attributes
     
     for p in getChildrenByTagName(ps, 'p'):
-        if p.getAttribute('align'):
-            if p.getAttribute('align') == 'right' or p.getAttribute('align') == 'center':
-                attributes += ' class="aligned" align="' + p.getAttribute('align') + '"'
+        align = p.getAttribute('align')
+        if align:
+            if align == 'right' or align == 'center':
+                attributes += ' class="aligned" align="' + align + '"'
             else:
                 exit('Error: Unknown <p> align position')
                 
@@ -311,7 +386,8 @@ def handlePChildren(c):
         if child.nodeType == 3:
             pChildrenOutput.append(child.nodeValue)
         elif child.nodeType == 1:
-            if child.nodeName == 'b' or child.nodeName == 'em' or child.nodeName == 'i' or child.nodeName == 'strong' or child.nodeName == 'sup' or child.nodeName == 'sub' or child.nodeName == 'u':
+            nodeNames = ['b', 'em', 'i', 'strong', 'sup', 'sub', 'u']
+            if child.nodeName in nodeNames:
                 pChildrenOutput.append('<' + child.nodeName + '>')
                 try:
                     if child.childNodes:
@@ -344,7 +420,10 @@ def handlePChildren(c):
                 except:
                     footnotes[footnote_id] = handlePChildren(child.childNodes)
                 
-                pChildrenOutput.append('<sup><a id="footnote_' + str(footnote_id) + '_backlink" href="#footnote_' + str(footnote_id) + '">[' + str(len(footnotes)) + ']</a></sup>')
+                pChildrenOutput.append('<sup><a id="footnote_' + 
+                        str(footnote_id) + '_backlink" href="#footnote_' + 
+                        str(footnote_id) + '">[' + 
+                        str(len(footnotes)) + ']</a></sup>')
             elif child.nodeName == 'img':
                 try:
                     imgname = child.getAttribute('name')
@@ -377,7 +456,10 @@ def returnFootnotes():
         footnotesOutput.append('<hr />')
         i = 1
         for k, v in footnotes.iteritems():
-            footnotesOutput.append('<p class="outdent"><a id="footnote_' + str(k) + '" href="#footnote_' + str(k) + '_backlink">' + str(i) + '</a> ' + v + '<br /><br /></p>')
+            footnotesOutput.append('<p class="outdent"><a id="footnote_' + 
+                    str(k) + '" href="#footnote_' + 
+                    str(k) + '_backlink">' + 
+                    str(i) + '</a> ' + v + '<br /><br /></p>')
             i = i + 1
         
         footnotes = {}
@@ -430,20 +512,20 @@ def main():
 
         # Process books
         for book in getChildrenByTagName(topLevelElement, 'book'):
+            try:
+                titleElement = getChildrenByTagName(book, 'title')
+                title = handlePChildren(titleElement[0].childNodes)
+            except:
+                exit('Error: Missing book <title> tag')
+            
             # Book num and title
             if book.getAttribute('num'):
-                output.append('<h1 class="book_title">Book ' + book.getAttribute('num') + '</h1>')
-                try:
-                    output.append('<p class="subtitle">' + handlePChildren(getChildrenByTagName(book, 'title')[0].childNodes) + '</p>')
-                except:
-                    exit("Error: Missing book <title> tag")
+                output.append('<h2>Book ' + book.getAttribute('num') + '</h2>')
+                output.append('<p class="subtitle">' + title + '</p>')
 
             # Book title on its own
             else:
-                try:
-                    output.append('<h1>' + handlePChildren(getChildrenByTagName(book, 'title')[0].childNodes) + '</h1>')
-                except:
-                    exit("Error: Missing book <title> tag")
+                output.append('<h2>Book ' + title + '</h2>')
 
             # Book subtitle if it exists
             subtitle = getChildrenByTagName(book, 'subtitle')
@@ -468,7 +550,7 @@ def main():
             exit("Error: Missing top level <volume> or <book> tag")
 
     # HTML footer
-    output.append("\n</body>\n</html>")
+    output.append('\n</body>\n</html>')
 
     # Write output to HTML file in same directory as XMLB file
     out_path = os.path.dirname(in_path) + "/" + string.split(os.path.basename(in_path), ".")[0] + ".html"
